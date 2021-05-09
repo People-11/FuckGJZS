7d821da2dc9bde5898270e119885736468b1714e
People-11 <349504120p@gmail.com>
2021-05-10 03:50:03 +0800

    Update

--- Util_Functions.sh
+++ Util_Functions.sh
@@ -18,8 +18,8 @@ export Status=$Data_Dir/Status.log
 export Termux=$DATA_DIR/com.termux/files
 export BOOTMODE=true
 export Choice=0
-export New_Version=2.10.11
-export New_Code=2021050802
+export New_Version=3
+export New_Code=2021051002
 export ChongQi Configuration File File_Name Download_File File_MD5 id name version versionCode author description MODID MODNAME MODPATH MAGISK_VER MAGISK_VER_CODE LOCKED
 $Have_ROOT && LOCKED=false || LOCKED=true
 
