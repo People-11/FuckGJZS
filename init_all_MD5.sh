cea9a894dac0cba43b31c1110c660f438068ec45
People-11 <41558215+People-11@users.noreply.github.com>
2021-05-28 14:31:08 +0800

    Try to fix

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=78c22c88fff8faff03b85da318bd8ca5
+    init_data_MD5=d85587f7dba6d5f63acba09e1818c35b
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=5cc0074d868e5fcf0702b0379893ae89
     Cloud_ID=Cloud_Page.zip
