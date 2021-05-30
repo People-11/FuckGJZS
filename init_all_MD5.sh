640378f736491e88737cc2f9145b4b581105c997
People-11 <349504120p@gmail.com>
2021-05-31 02:46:28 +0800

    去除掉一个还没开发的功能

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=14e3392c6046f163577f5992f30e99d0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=e5dce1754cb9da37a740338242ab9310
+    Cloud_MD5=3c5e778a06b82453af1ce73fc29aad9f
     Cloud_Version=2021053108
