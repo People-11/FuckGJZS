1d3d040fe98a0ed784e24092bc0a7ecdfa3a9de6
People-11 <349504120p@gmail.com>
2021-05-30 23:43:40 +0800

    Optimize Typo

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=e3cfbc0c12be68f0c1d58cf6844d85b7
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=6946c2f14a0685388c889d27bcd8ed55
+    Cloud_MD5=7eba74403e18826f0b22bae0042a70d4
     Cloud_Version=2021053019
