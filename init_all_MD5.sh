431010b8c0916ff7a9338084880850bd791b8f4c
People-11 <349504120p@gmail.com>
2021-05-31 18:49:21 +0800

    小狐狸与败家之眼动画出现问题，先下架了

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=14e3392c6046f163577f5992f30e99d0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=06d93820e3bde624078c349eb4c09b4c
+    Cloud_MD5=2d9c9c61bb4f24fd2ce6f38b8cdb87e7
     Cloud_Version=2021060108
