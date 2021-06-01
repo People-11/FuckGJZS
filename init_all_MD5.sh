8bbff7f84d7b8fe5bd3e221c724360cb19e8a415
People-11 <349504120p@gmail.com>
2021-06-01 11:37:25 +0800

    界面与功能调整

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=3da89dd9272a6b197598b49a4dcb61fa
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=69df7b889be3f7ca8e689beb17c91c0a
+    Cloud_MD5=647a0ac85f46c700dba663a2ea61c74b
     Cloud_Version=2021060108
