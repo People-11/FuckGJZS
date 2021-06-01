b8f53f9830cea7d602f91d171d969db7523dda78
People-11 <349504120p@gmail.com>
2021-06-01 11:56:00 +0800

    小调整

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=e9af2f2190b8c218ec85419748c795df
+    init_data_MD5=e6b82d5d7830a992dcc517423ca1fdf8
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=3da89dd9272a6b197598b49a4dcb61fa
     Cloud_ID=Cloud_Page.zip
