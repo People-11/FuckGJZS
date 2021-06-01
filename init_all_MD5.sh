0050b1632392af6afa9aaf2bc8e908e22fc8cc5d
People-11 <349504120p@gmail.com>
2021-06-01 12:03:05 +0800

    改错了

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,7 +1,7 @@
     init_data_ID=init_data.sh
-    init_data_MD5=e6b82d5d7830a992dcc517423ca1fdf8
+    init_data_MD5=e9af2f2190b8c218ec85419748c795df
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=3da89dd9272a6b197598b49a4dcb61fa
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=647a0ac85f46c700dba663a2ea61c74b
+    Cloud_MD5=e6b82d5d7830a992dcc517423ca1fdf8
     Cloud_Version=2021060108
