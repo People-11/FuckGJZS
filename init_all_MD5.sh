a75bf36259257b6bfdab431da1d78292fd0d06f4
People-11 <349504120p@gmail.com>
2021-06-07 17:32:30 +0800

    去掉了一个完全不会生效的功能

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=6390e0a60d5c772766e2bbc69f306f27
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=bca0976d44740b807a1fd75108670aa1
-    Cloud_Version=2021060418
+    Cloud_MD5=e261988e025ae3e5706e0a6474955b7d
+    Cloud_Version=2021060718
