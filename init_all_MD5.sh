17f274e4774072428d45ca8c312e5f91b0f6d8b6
People-11 <349504120p@gmail.com>
2021-06-19 05:38:13 +0800

    哪个屑把我换行符换成CRLF了

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,7 +1,7 @@
     init_data_ID=init_data.sh
-    init_data_MD5=194ed6d759810d52f090cb22262681eb
+    init_data_MD5=a038ae4b8f5c388e2bc08103a7121221
     Util_Functions_ID=Util_Functions.sh
-    Util_Functions_MD5=9e1e69c5b90398323652084d03b8f1c8
+    Util_Functions_MD5=26beed466e008cce2120d3e1ec237811
     Cloud_ID=Cloud_Page.zip
     Cloud_MD5=8fc1cbf50117a674531c0ba82a93f023
     Cloud_Version=2021061800
