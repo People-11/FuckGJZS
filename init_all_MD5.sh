f50a87eb1e5a120a250a6f17fb5e28ba46671e7b
People-11 <349504120p@gmail.com>
2021-06-19 05:30:11 +0800

    Remove Lanzou Link Download Support

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,7 +1,7 @@
     init_data_ID=init_data.sh
     init_data_MD5=a038ae4b8f5c388e2bc08103a7121221
     Util_Functions_ID=Util_Functions.sh
-    Util_Functions_MD5=bc191f3d670d11196e0b042c53520c7e
+    Util_Functions_MD5=9e1e69c5b90398323652084d03b8f1c8
     Cloud_ID=Cloud_Page.zip
     Cloud_MD5=8fc1cbf50117a674531c0ba82a93f023
     Cloud_Version=2021061800
