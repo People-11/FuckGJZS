987a5b8e022ce901faa01b2406594a9ca8440572
People-11 <349504120p@gmail.com>
2021-06-19 05:33:38 +0800

    草了，我次次都忘记校验MD5

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=a038ae4b8f5c388e2bc08103a7121221
+    init_data_MD5=194ed6d759810d52f090cb22262681eb
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=9e1e69c5b90398323652084d03b8f1c8
     Cloud_ID=Cloud_Page.zip
