11ecb273a1ee50432ce033d9334c1c8079b91614
People-11 <349504120p@gmail.com>
2021-06-19 16:19:46 +0800

    调整布局

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=26beed466e008cce2120d3e1ec237811
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=8fc1cbf50117a674531c0ba82a93f023
+    Cloud_MD5=4d9d6789e619ba73ab6def003dbf50ab
     Cloud_Version=2021061800
