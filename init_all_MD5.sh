dad8ba813c1785935c9d6389cb0349ade260cbf1
People-11 <349504120p@gmail.com>
2021-06-22 00:38:26 +0800

    Fix Typo

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=26beed466e008cce2120d3e1ec237811
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=bd58159fafb9c3d90ae806a6acdfce4d
+    Cloud_MD5=774195c5665ab08d630ca896289d7a2e
     Cloud_Version=2021062201
