3740e105e1ce8a4ef2b3adb3b912a61818c99d93
People-11 <349504120p@gmail.com>
2021-08-01 01:35:08 +0800

    修复品牌空白的问题

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=683b6e3f0c15af86fe47d5342116a5c5
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=d489b733c267526183602a020b02d8c2
+    Cloud_MD5=c53176e216eb2b4113f6553da3c667be
     Cloud_Version=2021080101
