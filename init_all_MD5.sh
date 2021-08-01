583f4d3afe9438ca3a3ee9af9970ad5d52d30064
People-11 <349504120p@gmail.com>
2021-08-01 16:07:24 +0800

    MIX3已经不行了，还是得MIXAlpha

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=683b6e3f0c15af86fe47d5342116a5c5
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=9eb6615ce98089fbe8ff40a5d32f2af1
+    Cloud_MD5=bdb520316150d68d06c749ee0fe47e90
     Cloud_Version=2021080115
