ff469ad799525b780796fcb94d6b87939ad1c16f
People-11 <349504120p@gmail.com>
2021-08-12 20:26:27 +0800

    难道要用config-sh吗

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=683b6e3f0c15af86fe47d5342116a5c5
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=52bd75ab11bdfe6015f427f7b5c4990a
+    Cloud_MD5=63d833b7894916db5cb291ab7820cd17
     Cloud_Version=2021081220
