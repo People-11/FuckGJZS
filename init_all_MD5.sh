a125472021c5ff167a91c6900f2b74ab0bfb2f45
People-11 <349504120p@gmail.com>
2021-08-17 23:20:15 +0800

    Update IQOO 8

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=683b6e3f0c15af86fe47d5342116a5c5
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=12f299afdcf6db0e7af1ca17f255239b
-    Cloud_Version=2021081701
+    Cloud_MD5=f61fbc4ce92ca95f4e762346563d69c0
+    Cloud_Version=2021081723
