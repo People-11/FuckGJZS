f69a223d758eef7eb2a0d8796388d9017cb6df79
People-11 <349504120p@gmail.com>
2021-08-30 14:10:30 +0800

    Small Fix
    Fix Module Check Can Not Unzip Issue

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=683b6e3f0c15af86fe47d5342116a5c5
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=909c6287fedfff9107106e562d6ad4dc
+    Cloud_MD5=502915f1274c48a018bdc9ef2cb095d5
     Cloud_Version=2021082523
