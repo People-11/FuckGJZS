aa09dc8e882d013e76c370ebaf72fd96eb65372c
People-11 <349504120p@gmail.com>
2021-09-17 15:57:40 +0800

    Test Fix

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=f7cf5d1c5ff3be7d4489a6489c78cc44
+    init_data_MD5=a9794aa20d05fb5a4f9fb547ea3f37d0
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=683b6e3f0c15af86fe47d5342116a5c5
     Cloud_ID=Cloud_Page.zip
