a394040576c59a48b513c43d36b9eb18a416f60e
People-11 <349504120p@gmail.com>
2021-12-27 21:48:33 +0800

    Small Adjustment

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=395c7dc5e8b0587134d56748af1666b0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=b7dddafdd947b710f35aaa44fe392792
+    Cloud_MD5=12e5823657c5a04b220ee39ed5a8c1ff
     Cloud_Version=2021122721
