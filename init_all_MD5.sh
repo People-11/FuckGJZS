7a5fe7721f7c49b0a5951a415840aa364a67254c
People-11 <349504120p@gmail.com>
2021-12-28 03:24:49 +0800

    原来是加了反斜杠

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=395c7dc5e8b0587134d56748af1666b0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=70eda0685047015a3c19d5bc16aba681
-    Cloud_Version=2021122802
+    Cloud_MD5=76f09b6dcde9d43576c9a536194a0682
+    Cloud_Version=2021122803
