475c00dcaf261916c8f227f3ba1ac425ab10ccb1
People-11 <349504120p@gmail.com>
2021-12-28 03:37:50 +0800

    怎么是DemoMode

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=395c7dc5e8b0587134d56748af1666b0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=9693166c74a8278f66a9e6596815ef6c
+    Cloud_MD5=0b6dd4fee3a2db02e27ece17764e77f6
     Cloud_Version=2021122803
