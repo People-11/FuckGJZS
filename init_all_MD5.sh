d3382ec7dcc656265ff9ff676798bb49c1f1a6b1
People-11 <349504120p@gmail.com>
2022-01-28 16:44:21 +0800

    Update
    Remove files instead of removing folder

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=395c7dc5e8b0587134d56748af1666b0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=0eee364d36c3bb5c9fef475ba583170c
-    Cloud_Version=2022012604
+    Cloud_MD5=e5f150309a8a2df7729878365c8d8dce
+    Cloud_Version=2022012816
