7616b4c9b9ebd0ab11fa5d578aae3698375a7d42
People-11 <349504120p@gmail.com>
2022-01-29 00:01:33 +0800

    Update
    So much typo

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=395c7dc5e8b0587134d56748af1666b0
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=625b3a5f2d09bf136aab6787976c6012
-    Cloud_Version=2022012825
+    Cloud_MD5=d09aa8b339bed45386d21952bdc7b07e
+    Cloud_Version=2022012826
