f6986036bdb52f75d04abfa465b0c48ca1e39c42
People-11 <349504120p@gmail.com>
2022-04-07 19:20:38 +0800

    Update
    优化SDK判断流程

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=09574beeff5f117c1364684e3a35f330
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=cbaf455ee921cac97991450d0dff2e51
-    Cloud_Version=2022040613
+    Cloud_MD5=327d1d26ca3ce2da74b88916dd4609d2
+    Cloud_Version=2022040719
