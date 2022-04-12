dd6c6b97ae0bf9712f6ea757aa0bcd409e118113
People-11 <349504120p@gmail.com>
2022-04-13 03:12:44 +0800

    这下逻辑对了

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,7 +1,7 @@
     init_data_ID=init_data.sh
     init_data_MD5=a20de834baa98c2e52113292a891ca05
     Util_Functions_ID=Util_Functions.sh
-    Util_Functions_MD5=8c01eb798dd0400e5feacd09076c4a4a
+    Util_Functions_MD5=88d49b0863ce7ea96eb63f6f1e58829a
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=661b032ca62748ac2983292319215ad5
+    Cloud_MD5=d2d6cf2c01f13ac77c5b76d631fa54b6
     Cloud_Version=2022041302
