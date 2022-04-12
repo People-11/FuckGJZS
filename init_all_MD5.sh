6b79f56c1c41103cd746bfe737f8ea0099fba830
People-11 <349504120p@gmail.com>
2022-04-13 01:17:25 +0800

    Revert "Update"
    This reverts commit 65d41623e1d4099b2cbc98b219e43c0ded7a4e5a.

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=5e60b3d3dbad0c37d7edf30a722a7d37
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=0f371b9f6c30678c54ae6e402d388333
-    Cloud_Version=2022041301
+    Cloud_MD5=b566dcb0f07e653fef27a90e7e5780a5
+    Cloud_Version=2022041300
