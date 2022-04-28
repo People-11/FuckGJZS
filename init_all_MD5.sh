7bb1cfd676dd51aaeb5bf73c8e6804f2c44ea33b
People-11 <349504120p@gmail.com>
2022-04-29 00:31:37 +0800

    Shenmegui

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=88d49b0863ce7ea96eb63f6f1e58829a
     Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=78e549523df177cf8ab82d708c9cda60
+    Cloud_MD5=84daa5990694b760189ad2d6221d1190
     Cloud_Version=2022042900
