f2e0f2d6720cf603500211d5c887c9069c79bd23
People-11 <349504120p@gmail.com>
2022-07-04 03:43:57 +0800

    Shit

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,5 +1,5 @@
     init_data_ID=init_data.sh
-    init_data_MD5=61674a215f58e930e9a821912f8afad5
+    init_data_MD5=6349c7ee994712893ebcf25b883f6b59
     Util_Functions_ID=Util_Functions.sh
     Util_Functions_MD5=7ef7ce99804938ad80ae6bc793359a5e
     Cloud_ID=Cloud_Page.zip
