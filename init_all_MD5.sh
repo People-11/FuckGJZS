638489878033ab94ab7650bc5514de4855aa8e4e
People-11 <349504120p@gmail.com>
2022-08-24 17:32:26 +0800

    Try fix maybe already fixed newline error
    That's weird.

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,7 +1,7 @@
-    init_data_ID=init_data.sh
-    init_data_MD5=6349c7ee994712893ebcf25b883f6b59
-    Util_Functions_ID=Util_Functions.sh
-    Util_Functions_MD5=7ef7ce99804938ad80ae6bc793359a5e
-    Cloud_ID=Cloud_Page.zip
-    Cloud_MD5=191d5421486c70cae907cdde9c488e1f
-    Cloud_Version=2022082424
+init_data_ID=init_data.sh
+init_data_MD5=6349c7ee994712893ebcf25b883f6b59
+Util_Functions_ID=Util_Functions.sh
+Util_Functions_MD5=7ef7ce99804938ad80ae6bc793359a5e
+Cloud_ID=Cloud_Page.zip
+Cloud_MD5=191d5421486c70cae907cdde9c488e1f
+Cloud_Version=2022082424
