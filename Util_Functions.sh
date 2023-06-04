9dcf038ef44eb5f4ec1be5578c63b24f7b6ce62e
People-11 <349504120p@gmail.com>
2023-06-05 06:51:50 +1000

    Update
    忘记改了

--- Util_Functions.sh
+++ Util_Functions.sh
@@ -1,5 +1,5 @@
 #Custom variable
-export Util_Functions_Code=2022070403
+export Util_Functions_Code=2023060506
 export SDdir=/data/media/0
 export Modules_Dir=/data/adb/modules
 export Script_Dir=$TMPDIR/tmp
@@ -18,8 +18,8 @@ export Status=$Data_Dir/Status.log
 export Termux=$DATA_DIR/com.termux/files
 export BOOTMODE=true
 export Choice=0
-export New_Version=7
-export New_Code=2022041812
+export New_Version=7.1
+export New_Code=2022052115
 export ChongQi Configuration File File_Name Download_File File_MD5 id name version versionCode author description MODID MODNAME MODPATH MAGISK_VER MAGISK_VER_CODE LOCKED
 
 
--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -1,7 +1,7 @@
 init_data_ID=init_data.sh
 init_data_MD5=3300b0d487b149e3bf2d1051421ceb98
 Util_Functions_ID=Util_Functions.sh
-Util_Functions_MD5=7ef7ce99804938ad80ae6bc793359a5e
+Util_Functions_MD5=da5930b02342b1420ae50fc9ef219fb2
 Cloud_ID=Cloud_Page.zip
 Cloud_MD5=3333b1e7c09b8c9a17d6db3b8c952896
 Cloud_Version=2023060506
