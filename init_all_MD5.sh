08ef62a9f1ca9f8608cbda361c25704832a00794
People-11 <349504120p@gmail.com>
2023-06-27 23:01:11 +1000

    Update

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@ init_data_MD5=3300b0d487b149e3bf2d1051421ceb98
 Util_Functions_ID=Util_Functions.sh
 Util_Functions_MD5=da5930b02342b1420ae50fc9ef219fb2
 Cloud_ID=Cloud_Page.zip
-Cloud_MD5=3333b1e7c09b8c9a17d6db3b8c952896
-Cloud_Version=2023060506
+Cloud_MD5=6a1b4bbe3103327cba4c41b33e094c1a
+Cloud_Version=2023062723
