fda5c2f83bb0fb55c4b3b6576ac20a7991c5bbea
People-11 <349504120p@gmail.com>
2023-12-01 23:15:50 +1100

    Fix Typo

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@ init_data_MD5=9979053912e31c22f3c3202b323c1b89
 Util_Functions_ID=Util_Functions.sh
 Util_Functions_MD5=9093c0af85213a821394fff86d97d20b
 Cloud_ID=Cloud_Page.zip
-Cloud_MD5=51c1e50cdd10721a84232d3b1fc3a93f
-Cloud_Version=2023112404
+Cloud_MD5=c724b7ee87b254d78d0634f2022179e4
+Cloud_Version=2023120123
