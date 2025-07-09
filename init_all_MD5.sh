613e129d502bc83dadbac2bf6c60a557cdb6c4f8
People-11 <41558215+People-11@users.noreply.github.com>
2025-07-09 10:40:23 +0930

    Update

--- init_all_MD5.sh
+++ init_all_MD5.sh
@@ -3,5 +3,5 @@ init_data_MD5=e80db6acecbf7004d86f09c0f7570814
 Util_Functions_ID=Util_Functions.sh
 Util_Functions_MD5=212f1b2b7c8cb925e3c6d3e60b44b8de
 Cloud_ID=Cloud_Page.zip
-Cloud_MD5=3fe392541c19c3fcb6f44806929edfe5
-Cloud_Version=2025042121
+Cloud_MD5=2139ce0b9f45f45f85d7c250d400b843
+Cloud_Version=2025070910
